9aecb00a98c06839b085473d1a4164370904aef9
Patrick O Neill <patrickonei@gmail.com>
2019-05-30 20:15:29 +0000

    created the merchandise page and the models for adding items

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Merchandise
 
 # Create your views here.
+
+def all_merch(request):
+    merch = Merchandise.objects.all()
+    return render(request, 'merchandise.html', {'merch': merch})
