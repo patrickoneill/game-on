da138ce88b03d2414791e4102715244856876c30
Patrick O Neill <patrickonei@gmail.com>
2019-05-30 20:46:20 +0000

    added travis

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_N00KKdIMATsuHdhbTe4nti97')
+os.environ.setdefault('STRIPE_SECRET', 'sk_test_M3Jq071MHsNdy5bKMK49yEok')
+os.environ.setdefault('DATABASE_URL', 'postgres://xoqagzazcgtmde:b9351d56f3afbd0380a3c34ac40cc436448a56e9223c8e773943299430002178@ec2-54-228-243-238.eu-west-1.compute.amazonaws.com:5432/d8e60nlvfl8r06')
+os.environ.setdefault('SECRET_KEY', 'i(w-%@35ba3y972^@&b3+y%o@&3%g118ng87yhh04$_-uti_(f')
+os.environ.setdefault('AWS_ACCESS_KEY_ID', 'AKIAWERL6AMCIT3NWSPV')
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY', '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')
